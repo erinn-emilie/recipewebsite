c56b2263dd47f0acaf374fd23b10d3bf4d25093a
erinn-emilie <erinnemiliek@gmail.com>
2025-08-23 17:00:54 -0400

    Added functionality to highlight and strikethrough steps and ingredients and added recipe name header. Can now for sure parse three websites, though the code def needs to be cleaned up and optimized. Might work for other websites.

--- src/Components/RecipeCard.jsx
+++ src/Components/RecipeCard.jsx
@@ -4,15 +4,44 @@ import { useState } from 'react'
 
 const RecipeCard = (props) => {
 
-    const [highlightedStep, setHighlighted] = useState(-1)
+    const [highlightedStep, setHighlightedStep] = useState(-1)
+    const [highlightedIng, setHighlightedIng] = useState(-1)
+    const [finishedSteps, setFinishedSteps] = useState([])
+    const [finishedIngs, setFinishedIngs] = useState([])
 
 
     const selectStep = (id) => {
-        setHighlighted(id)
+        setHighlightedStep(id)
     }
 
-    const deselectStep = () => {
-        setHighlighted(-1)
+    const finishStep = (id) => {
+        setHighlightedStep(-1)
+        let temp = finishedSteps
+        temp.push(id)
+        setFinishedSteps(temp)
+    }
+
+    const startStep = (id) => {
+        let temp = finishedSteps
+        temp = temp.filter(num => num !== id)
+        setFinishedSteps(temp)
+    }
+
+    const selectIng = (id) => {
+        setHighlightedIng(id)
+    }
+
+    const finishIng = (id) => {
+        setHighlightedIng(-1)
+        let temp = finishedIngs
+        temp.push(id)
+        setFinishedIngs(temp)
+    }
+
+    const startIng = (id) => {
+        let temp = finishedIngs
+        temp = temp.filter(num => num !== id)
+        setFinishedIngs(temp)
     }
 
     if (props.data.ingredients === undefined) {
@@ -24,42 +53,66 @@ const RecipeCard = (props) => {
     }
 
     return (
-        <div className="rounded recipe--card container-fluid d-flex flex-row">
-            <div className="p-2 d-flex flex-column container-fluid ingredients--column mt-3">
-                <h2 className="header">Ingredients</h2>
-                <div className="p-2 d-flex">Yield: {props.data?.yield}</div>
-                {
-                    props.data.ingredients?.map((ingredient) => (
-                        <div className="p-2 d-flex">{ingredient}</div>
-                    ))
-                }
-            </div>
-            <div className="p-2 d-flex flex-column container-fluid instructions--column mt-3">
-                <h2 className="header">Instructions</h2>
-                <div className="p-2 d-flex">Cook Time: {props.data?.cook_time}</div>
-                {
-                    props.data.instructions?.map((step, number) => (
-                        <>
-                            {
-                                number == highlightedStep && (
-                                    <div onClick={() => deselectStep()} className="p-2 d-flex rounded selectedstep mb-3 mt-3">Step {number + 1}. {step}</div>
-                                )
-                            }
-                            {
-                                number != highlightedStep && (
-                                    <div onClick={() => selectStep(number)} className="p-2 d-flex">Step {number + 1}. {step}</div>
-                                )
-                            }
-                        </>
-                    ))
-                }
-            </div>
-            <div className="p-2 d-flex flex-column container-fluid info--column mt-3">
-                <h2 className="header">Other Info</h2>
-                <div className="p-2 d-flex">Author: {props.data?.author}</div>
-                <div className="p-2 d-flex">Cuisine Style: {props.data?.cuisine}</div>
-                <div className="p-2 d-flex">Category: {props.data?.category}</div>
-                <button className="btn btn-primary">Save Recipe</button>
+        <div className="rounded recipe--card container-fluid">
+            <h1 className="d-flex flex-row recipe--header mt-3">{props.data?.name}</h1>
+            <div className="d-flex flex-row">
+                <div className="p-2 d-flex flex-column container-fluid ingredients--column">
+                    <h2 className="header">Ingredients</h2>
+                    <div className="p-2 d-flex">Yield: {props.data?.yield}</div>
+                    {
+                        props.data.ingredients?.map((ingredient, number) => (
+                            <>
+                                {
+                                    number == highlightedIng && (
+                                        <div onClick={() => finishIng(number)} className="p-2 d-flex rounded selected mb-3 mt-3">{ingredient}</div>
+                                    )
+                                }
+                                {
+                                    (number != highlightedIng && finishedIngs.indexOf(number) == -1) && (
+                                        <div onClick={() => selectIng(number)}  className="p-2 d-flex">{ingredient}</div>
+                                    )
+                                }
+                                {
+                                    finishedIngs.indexOf(number) != -1 && (
+                                        <div onClick={() => startIng(number)} className="p-2 d-flex strickthrough">{ingredient}</div>
+                                    )
+                                }
+                            </>
+                        ))
+                    }
+                </div>
+                <div className="p-2 d-flex flex-column container-fluid instructions--column">
+                    <h2 className="header">Instructions</h2>
+                    <div className="p-2 d-flex">Cook Time: {props.data?.cook_time}</div>
+                    {
+                        props.data.instructions?.map((step, number) => (
+                            <>
+                                {
+                                    number == highlightedStep && (
+                                        <div onClick={() => finishStep(number)} className="p-2 d-flex rounded selected mb-3 mt-3">Step {number + 1}. {step}</div>
+                                    )
+                                }
+                                {
+                                    (number != highlightedStep && finishedSteps.indexOf(number) == -1) && (
+                                        <div onClick={() => selectStep(number)} className="p-2 d-flex">Step {number + 1}. {step}</div>
+                                    )
+                                }
+                                {
+                                    finishedSteps.indexOf(number) != -1 && (
+                                        <div onClick={() => startStep(number)} className="p-2 d-flex strickthrough">Step {number + 1}. {step}</div>
+                                    )
+                                }
+                            </>
+                        ))
+                    }
+                </div>
+                <div className="p-2 d-flex flex-column container-fluid info--column">
+                    <h2 className="header">Other Info</h2>
+                    <div className="p-2 d-flex">Author: {props.data?.author}</div>
+                    <div className="p-2 d-flex">Cuisine Style: {props.data?.cuisine}</div>
+                    <div className="p-2 d-flex">Category: {props.data?.category}</div>
+                    <button className="btn btn-primary">Save Recipe</button>
+                </div>
             </div>
         </div>
     )
